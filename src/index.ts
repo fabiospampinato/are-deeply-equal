f96f5a0fa5a03015f4933016091f0fc48d40dfe2
Fabio Spampinato <spampinabio@gmail.com>
2022-11-05 12:46:02 +0000

    Internal reorganization, much better structured

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,114 @@ const Node = globalThis.Node;
 
 /* MAIN */
 
-const areDeeplyEqualInner = ( a: any, b: any, _compareMap: Map<unknown, unknown> ): boolean => {
+const isEqualArray = ( a: unknown[], b: unknown[], _compareMap: Map<unknown, unknown> ): boolean => {
+
+  if ( a.length !== b.length ) return false;
+
+  for ( let i = a.length - 1; i >= 0; i-- ) {
+
+    if ( !isEqualGeneral ( a[i], b[i], _compareMap ) ) return false;
+
+  }
+
+  return true;
+
+};
+
+const isEqualMap = ( a: Map<unknown, unknown>, b: Map<unknown, unknown>, _compareMap: Map<unknown, unknown> ): boolean => {
+
+  if ( a.size !== b.size ) return false;
+
+  for ( const [key, valueA] of a.entries () ) {
+
+    const valueB = b.get ( key );
+
+    if ( !isEqualGeneral ( valueA, valueB, _compareMap ) ) return false;
+
+    if ( valueB === undefined && !b.has ( key ) ) return false;
+
+  }
+
+  return true;
+
+};
+
+const isEqualSet = ( a: Set<unknown>, b: Set<unknown>, _compareMap: Map<unknown, unknown> ): boolean => {
+
+  if ( a.size !== b.size ) return false;
+
+  for ( const [valueA] of a.entries () ) {
+
+    if ( !b.has ( valueA ) ) return false;
+
+  }
+
+  return true;
+
+};
+
+const isEqualDate = ( a: Date, b: Date ): boolean => {
+
+  return is ( a.getTime (), b.getTime () );
+
+};
+
+const isEqualRegExp = ( a: RegExp, b: RegExp ): boolean => {
+
+  return a.source === b.source && a.flags === b.flags;
+
+};
+
+const isEqualArrayBuffer = ( a: ArrayBuffer, b: ArrayBuffer ): boolean => {
+
+  return isEqualTypedArray ( new Uint8Array ( a ), new Uint8Array ( b ) );
+
+};
+
+const isEqualTypedArray = <T extends Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | BigInt64Array | BigUint64Array> ( a: T, b: T ): boolean => {
+
+  if ( a.length !== b.length ) return false;
+
+  for ( let i = a.length - 1; i >= 0; i-- ) {
+
+    if ( a[i] !== b[i] ) return false;
+
+  }
+
+  return true;
+
+};
+
+const isEqualValueOf = ( a: object, b: object, _compareMap: Map<unknown, unknown> ): boolean => {
+
+  return isEqualGeneral ( a.valueOf (), b.valueOf (), _compareMap );
+
+};
+
+const isEqualObject = ( a: Record<string | number | symbol, unknown>, b: Record<string | number | symbol, unknown>, _compareMap: Map<unknown, unknown> ): boolean => {
+
+  const propertiesA = keys ( a );
+  const propertiesB = keys ( b );
+
+  if ( propertiesA.length !== propertiesB.length ) return false;
+
+  for ( let i = propertiesA.length - 1; i >= 0; i-- ) {
+
+    const property = propertiesA[i];
+    const valueA = a[property];
+    const valueB = b[property];
+
+    if ( !isEqualGeneral ( valueA, valueB, _compareMap ) ) return false;
+
+    if ( valueB === undefined && !hasOwnProperty.call ( b, property ) ) return false;
+
+  }
+
+  return true;
+
+};
+
+const isEqualGeneral = ( a: any, b: any, _compareMap: Map<unknown, unknown> ): boolean => {
 
   if ( is ( a, b ) ) return true;
 
@@ -24,74 +131,31 @@ const areDeeplyEqualInner = ( a: any, b: any, _compareMap: Map<unknown, unknown>
 
     if ( constructor === Array ) {
 
-      const length = a.length;
-
-      if ( length !== b.length ) return false;
-
-      for ( let i = length - 1; i >= 0; i-- ) {
-
-        if ( !areDeeplyEqualInner ( a[i], b[i], _compareMap ) ) return false;
-
-      }
-
-      return true;
+      return isEqualArray ( a, b, _compareMap );
 
     } else if ( constructor === Map ) {
 
-      if ( a.size !== b.size ) return false;
-
-      for ( const [key, valueA] of a.entries () ) {
-
-        const valueB = b.get ( key );
-
-        if ( !areDeeplyEqualInner ( valueA, valueB, _compareMap ) ) return false;
-
-        if ( valueB === undefined && !b.has ( key ) ) return false;
-
-      }
-
-      return true;
+      return isEqualMap ( a, b, _compareMap );
 
     } else if ( constructor === Set ) {
 
-      if ( a.size !== b.size ) return false;
-
-      for ( const [valueA] of a.entries () ) {
-
-        if ( !b.has ( valueA ) ) return false;
-
-      }
-
-      return true;
+      return isEqualSet ( a, b, _compareMap );
 
     } else if ( constructor === Date ) {
 
-      return is ( a.getTime (), b.getTime () );
+      return isEqualDate ( a, b );
 
     } else if ( constructor === RegExp ) {
 
-      return a.flags === b.flags && a.source === b.source;
+      return isEqualRegExp ( a, b );
 
-    } else if ( constructor === ArrayBuffer || isView ( a ) ) {
+    } else if ( constructor === ArrayBuffer ) {
 
-      if ( constructor === ArrayBuffer ) {
+      return isEqualArrayBuffer ( a, b );
 
-        a = new Uint8Array ( a );
-        b = new Uint8Array ( b );
+    } else if ( isView ( a ) ) {
 
-      }
-
-      const length = a.length;
-
-      if ( length !== b.length ) return false;
-
-      for ( let i = length - 1; i >= 0; i-- ) {
-
-        if ( a[i] !== b[i] ) return false;
-
-      }
-
-      return true;
+      return isEqualTypedArray ( a, b );
 
     } else if ( constructor === Promise || constructor === WeakMap || constructor === WeakSet || constructor === Node ) {
 
@@ -99,28 +163,11 @@ const areDeeplyEqualInner = ( a: any, b: any, _compareMap: Map<unknown, unknown>
 
     } else if ( a.valueOf !== valueOf ) {
 
-      return areDeeplyEqualInner ( a.valueOf (), b.valueOf (), _compareMap );
+      return isEqualValueOf ( a, b, _compareMap );
 
     } else {
 
-      const properties = keys ( a );
-      const length = properties.length;
-
-      if ( length !== keys ( b ).length ) return false;
-
-      for ( let i = length - 1; i >= 0; i-- ) {
-
-        const property = properties[i];
-        const valueA = a[property];
-        const valueB = b[property];
-
-        if ( !areDeeplyEqualInner ( valueA, valueB, _compareMap ) ) return false;
-
-        if ( valueB === undefined && !hasOwnProperty.call ( b, property ) ) return false;
-
-      }
-
-      return true;
+      return isEqualObject ( a, b, _compareMap );
 
     }
 
@@ -130,12 +177,12 @@ const areDeeplyEqualInner = ( a: any, b: any, _compareMap: Map<unknown, unknown>
 
 };
 
-const areDeeplyEqual = ( a: unknown, b: unknown ): boolean => {
+const isEqual = ( a: unknown, b: unknown ): boolean => {
 
-  return areDeeplyEqualInner ( a, b, new Map () );
+  return isEqualGeneral ( a, b, new Map () );
 
 };
 
 /* EXPORT */
 
-export default areDeeplyEqual;
+export default isEqual;
